99b385d08c64fc8a36ad5be17efd9d8f48ab6719
David <eth3rnit3@outlook.fr>
2019-03-05 16:23:39 +0100

    add prod url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,7 +1,7 @@
 const baseUrl = process.env.NODE_ENV === 'development' ?
 'http://127.0.0.1:3000/api/v1'
 :
-'http://127.0.0.1:3000/api/v1'
+'https://warm-castle-78155.herokuapp.com/api/v1'
 
 module.exports = {
   randomResult: `${baseUrl}/random_results`
